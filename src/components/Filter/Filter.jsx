5807e11080a74fc3e6ef9a9f639ee5b7046c77b9
KulibabaYuliia <kulibaba120@gmail.com>
2023-12-04 01:09:11 +0200

    filter with MUI

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,20 +1,38 @@
-import { FilterWrap } from './Filter.styled';
+// import { FilterWrap } from './Filter.styled';
 import { changeFilter } from 'redux/contacts/contacts.reducer';
 import { useDispatch } from 'react-redux';
+import { Box, TextField } from '@mui/material';
 
 export const Filter = () => {
   const dispatch = useDispatch();
 
   return (
-    <FilterWrap>
-      <label>
-        Find contact by name
-        <input
-          type="text"
-          name="filter"
-          onChange={e => dispatch(changeFilter(e.target.value))}
-        />
-      </label>
-    </FilterWrap>
+    // <FilterWrap>
+    //   <label>
+    //     Find contact by name
+    //     <input
+    //       type="text"
+    //       name="filter"
+    //       onChange={e => dispatch(changeFilter(e.target.value))}
+    //     />
+    //   </label>
+    // </FilterWrap>
+
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField
+        id="filled-search"
+        label="Search field"
+        type="search"
+        variant="filled"
+        onChange={e => dispatch(changeFilter(e.target.value))}
+      />
+    </Box>
   );
 };
